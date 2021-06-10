c8051f1da6198d78aa454144ee74a35041a48d2a
Harvindar Singh <41161615+Harvindar994@users.noreply.github.com>
2021-06-10 18:38:43 +0530

    Creating a Setting class to store the settings of the software

--- main.py
+++ main.py
@@ -6,3 +6,9 @@ import time
 from moviepy.editor import *
 import sys
 import pickle
+
+class settings:
+    def __init__(self):
+        self.filename = 'record.txt'
+        self.lastOuputPath = 'c:/'
+        self.lastFileLoadingPath = 'c:/'
