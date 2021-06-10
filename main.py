5bbfe716285dc5f00ece25dd84b2efd047fed4d8
Harvindar Singh <41161615+Harvindar994@users.noreply.github.com>
2021-06-10 18:35:37 +0530

    Add the icon and media files loaded and imported required packages

---
+++ main.py
@@ -0,0 +1,8 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+import os
+from PyQt5.QtCore import QObject, QEvent
+import clipboard
+import time
+from moviepy.editor import *
+import sys
+import pickle
